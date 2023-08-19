41b5eaa1f3919b417e250f39f838af754b4ce117
Amri <121174751+nature-modren@users.noreply.github.com>
2023-08-19 17:30:05 +0700

    Test:membuat file js tambahan untuk percobaan engine.js

---
+++ js/engine.js
@@ -0,0 +1,18 @@
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault(); // Mencegah refresh otomatis
+    Swal.fire({
+        title: 'Do you want to save the changes?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        denyButtonText: `Don't save`,
+    }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            Swal.fire('Saved!', '', 'success')
+            window.location.reload(); // Merefresh halaman saat user memilih untuk menyimpan perubahan
+        } else if (result.isDenied) {
+            Swal.fire('Changes are not saved', '', 'info')
+        }
+    });
+});
