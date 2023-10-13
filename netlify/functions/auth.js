9438395604d43ec3d7023bd4dbfc2ddddac45b42
Amri <121174751+nature-modren@users.noreply.github.com>
2023-10-13 21:51:24 +0700

    Update: menghapus fungsi membuat cookies auth.js
    cookies tidak bisa di buat di sisi server

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,32 +1,23 @@
 //build serverless netlify
 
-
- function setLoggedInCookie() {
-    const expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 1); // Cookie berlaku selama 1 hari
-
-    document.cookie = `loggedIn=true; expires=${expirationDate.toUTCString()}; path=/`;
-}
-
 exports.handler = async (event) => {
     if (event.httpMethod === "POST") {
         const requestBody = JSON.parse(event.body);
         const password = requestBody.password;
 
         if (password === "12345678") {
-            setLoggedInCookie(); // Panggil fungsi setLoggedInCookie di sini
             return {
                 statusCode: 200,
                 body: JSON.stringify({ message: "Password valid" }),
             };
         } else if (password === "friend") {
-            // Jika password adalah "friend", tampilkan pesan "hi"
+            
             return {
                 statusCode: 201,
                 body: JSON.stringify({ message: ":v ada ada saja beliau ini" }),
             };
         } else if (password === "password3") {
-            // Tambahkan kondisi untuk kata sandi lain di sini
+            
             return {
                 statusCode: 202,
                 body: JSON.stringify({ message: "Kata sandi lainnya" }),
