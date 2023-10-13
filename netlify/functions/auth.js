ff7236a5c3ce634a20266c3930d862db19372173
Amri <121174751+nature-modren@users.noreply.github.com>
2023-10-13 20:14:16 +0700

    Update: mengubah validasi di auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,35 +1,14 @@
 //build serverless netlify 
 
-/* exports.handler = async (event) => {
-  if (event.httpMethod === "POST") {
-    const requestBody = JSON.parse(event.body);
+function setLoggedInCookie(){ 
+const expirationDate = new Date(); 
 
-    // Di sini Anda dapat menambahkan logika validasi untuk password.
-    // Misalnya, Anda dapat memeriksa apakah password cocok dengan kriteria tertentu.
-    const password = requestBody.password;
+expirationDate.setDate(expirationDate.getDate() + 1); // Cookie berlaku selama 1 hari
 
-    // Misalnya, validasi password minimal harus memiliki 6 karakter.
-    if (password === "12345678") {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Password valid" }),
-      };
-    }
+  document.cookie = `loggedIn=true; expires=${expirationDate.toUTCString()}; path=/`;
+};
 
-    // Jika validasi berhasil, Anda dapat melakukan tindakan selanjutnya di sini, misalnya menyimpan data atau mengirim respons sukses.
-    // Gantilah bagian ini sesuai dengan kebutuhan Anda.
 
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Password invalid." }),
-    };
-  } else {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({ message: "Metode HTTP tidak didukung." }),
-    };
-  }
-}; */
 
 
 exports.handler = async (event) => { 
@@ -37,11 +16,13 @@ exports.handler = async (event) => {
      const requestBody = JSON.parse(event.body); 
   
      const password = requestBody.password; 
-  
+     
      if (password === "12345678") { 
+       setLoggedInCookie()
        return { 
          statusCode: 200, 
          body: JSON.stringify({ message: "Password valid" }), 
+        
        }; 
      } else if (password === "friend") {
        // Jika password adalah "friend", tampilkan pesan "hi"
@@ -62,3 +43,4 @@ exports.handler = async (event) => {
      }; 
    } 
  };
+
