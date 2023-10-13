3b847d117e631b183e2d2270cac324da15236731
Amri <121174751+nature-modren@users.noreply.github.com>
2023-10-13 20:54:01 +0700

    Debug: upaya penyelesaian auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -17,25 +17,29 @@ exports.handler = async (event) => {
   
      const password = requestBody.password; 
      
-     if (password === 12345678) { 
-       setLoggedInCookie()
-       return { 
-         statusCode: 200, 
-         body: JSON.stringify({ message: "Password valid" }), 
-        
-       }; 
-     } else if (password === "friend") {
-       // Jika password adalah "friend", tampilkan pesan "hi"
-       return { 
-         statusCode: 201, 
-         body: JSON.stringify({ message: ":v ada ada saja beliau ini" }), 
-       }; 
-     } else { 
-       return { 
-         statusCode: 400, 
-         body: JSON.stringify({ message: "Password invalid." }), 
-       }; 
-     } 
+     if (password === "12345678") {  
+    setLoggedInCookie() 
+    return {  
+        statusCode: 200,  
+        body: JSON.stringify({ message: "Password valid" }),  
+    };  
+} else if (password === "friend") { 
+    // Jika password adalah "friend", tampilkan pesan "hi" 
+    return {  
+        statusCode: 201,  
+        body: JSON.stringify({ message: ":v ada ada saja beliau ini" }),  
+    };  
+} else if (password === "password3") {
+    // Tambahkan kondisi untuk kata sandi lain di sini
+    return {
+        statusCode: 202,
+        body: JSON.stringify({ message: "Kata sandi lainnya" }),
+    };
+} else {
+    return {  
+        statusCode: 400,  
+        body: JSON.stringify({ message: "Password invalid." }),  
+    };  
    } else { 
      return { 
        statusCode: 405, 
