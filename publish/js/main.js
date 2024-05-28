9ec96d004c709a5fcee03de3bf23d5feb574508d
nature-modren <121174751+nature-modren@users.noreply.github.com>
2024-05-28 13:54:31 +0700

    minor change

--- publish/js/main.js
+++ publish/js/main.js
@@ -1,4 +1,3 @@
-// Pada halaman "Index-003.html"
 document.addEventListener("DOMContentLoaded", function () {
   if (!checkLoginStatus()) {
     // Jika pengguna belum login, alihkan ke halaman login atau tampilkan pesan.
@@ -12,3 +11,75 @@ function checkLoginStatus() {
   // Contoh sederhana: Periksa apakah cookie sesi "loggedIn" ada.
   return document.cookie.includes("loggedIn=true");
 }
+
+window.onload = function () {
+  openingwindow();
+
+  const openingwindow = () => {
+    let element = document.getElementById("textjudul");
+    if (element.classList.contains("animate__slideOutRight"))
+      element.classList.remove("animate__slideOutRight");
+    else {
+      element.classList.add("animate__slideInLeft");
+    }
+  };
+
+  const closingwindow = () => {
+    let element = document.getElementById("textjudul");
+    if (element.classList.contains("animate__slideInLeft")) {
+      element.classList.remove("animate__slideInLeft");
+    } else {
+      element.classList.add("animate__slideOutRight");
+    }
+  };
+
+  document
+    .getElementById("tautan1")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+
+      closingwindow();
+
+      setTimeout(function () {
+        window.location.href = "Index.html";
+      }, 1000);
+    });
+
+  document
+    .getElementById("tautan2")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+
+      closingwindow();
+
+      setTimeout(function () {
+        window.location.href = "koleksi.html";
+      }, 1000);
+    });
+
+  document
+    .getElementById("tautan3")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+
+      closingwindow();
+
+      setTimeout(function () {
+        window.location.href = "kontak.html";
+      }, 1000);
+    });
+};
+
+const text = "Hai semua saya Amri Firdaus,seorang Web developer";
+let index = 0;
+let speedketik = 100;
+
+function ketik() {
+  if (index < text.length) {
+    document.getElementById("text-animasi").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(ketik, speedketik);
+  }
+}
+
+ketik();
