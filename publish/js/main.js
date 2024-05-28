9c3331e954311d8dee7df7a35ce3d8ddd3d2497e
nature-modren <121174751+nature-modren@users.noreply.github.com>
2024-05-28 14:06:30 +0700

    Add function

--- publish/js/main.js
+++ publish/js/main.js
@@ -12,6 +12,51 @@ function checkLoginStatus() {
   return document.cookie.includes("loggedIn=true");
 }
 
+//File validation
+
+document
+  .getElementById("myform")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const password = document.getElementById("exampleInputPassword1").value;
+
+    function setLoggedInCookie() {
+      const expirationDate = new Date();
+      expirationDate.setDate(expirationDate.getDate() + 1); // Cookie berlaku selama 1 hari
+
+      document.cookie = `loggedIn=true; expires=${expirationDate.toUTCString()}; path=/`;
+    }
+
+    const response = await fetch(".netlify/functions/Auth", {
+      method: "POST",
+      body: JSON.stringify({ password }),
+    });
+
+    const responseData = await response.json();
+
+    console.log(responseData);
+
+    if (responseData.message === 200) {
+      setLoggedInCookie();
+      location.href = "halamanKoleksi.html";
+    } else if (responseData.message === 201) {
+      Swal.fire("Hi!", "Pesan: ada ada saja beliau ini kwkw", "success");
+    } else {
+      Swal.fire("Password Salah!", responseData.message, "error");
+    }
+  });
+
+const passwordField = document.getElementById("exampleInputPassword1");
+const showPasswordSwitch = document.getElementById("flexSwitchCheckDefault");
+
+showPasswordSwitch.addEventListener("change", function () {
+  if (showPasswordSwitch.checked) {
+    passwordField.type = "text";
+  } else {
+    passwordField.type = "password";
+  }
+});
+
 window.onload = function () {
   openingwindow();
 
